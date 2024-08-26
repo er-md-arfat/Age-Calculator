2d13c2fb8765764d9739baf4483e75c116eaa172
Mohammed Arfat Aslam Ansari <132613834+er-md-arfat@users.noreply.github.com>
2024-08-26 11:10:09 +0530

    Add files via upload

---
+++ age_cal1.php
@@ -0,0 +1,421 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Age Calculator</title>
+	</head>
+	
+	<body>
+		<b><p style='font-size:25px'>My First Website</p>
+	
+		<p><b><a style='font-size:20px;text-decoration:none;color:black;border:black solid 1px;padding:5px;' href='age_cal1.php'>Home</a>
+
+		<a style='font-size:20px;text-decoration:none;color:black;border:black solid 1px;padding:5px;' 
+		href='age_cal1.php'>AGE CALCULATOR</a>
+	
+		</b></p>
+		
+		<center>
+
+		<p><top style='font-size: 60px;'>AGE CALCULATOR</top></p>
+		<form method='POST' action='age_cal1.php'>
+			Day
+			<select name='day'>
+			<option>01</option>
+			<option>02</option>
+			<option>03</option>
+			<option>04</option>
+			<option>05</option>
+			<option>06</option>
+			<option>07</option>
+			<option>08</option>
+			<option>09</option>
+			<option>10</option>
+			<option>11</option>
+			<option>12</option>
+			<option>13</option>
+			<option>14</option>
+			<option>15</option>
+			<option>16</option>
+			<option>17</option>
+			<option>18</option>
+			<option>19</option>
+			<option>20</option>
+			<option>21</option>
+			<option>22</option>
+			<option>23</option>
+			<option>24</option>
+			<option>25</option>
+			<option>26</option>
+			<option>27</option>
+			<option>28</option>
+			<option>29</option>
+			<option>30</option>
+			<option>31</option>
+			</select>
+			 
+			Month
+			<select name='month'>
+			<option>01</option>
+			<option>02</option>
+			<option>03</option>
+			<option>04</option>
+			<option>05</option>
+			<option>06</option>
+			<option>07</option>
+			<option>08</option>
+			<option>09</option>
+			<option>10</option>
+			<option>11</option>
+			<option>12</option>
+			</select>
+
+			Year
+			<select name='year'>
+			<option>2024</option>
+			<option>2023</option>
+			<option>2022</option>
+			<option>2021</option>
+			<option>2020</option>
+			<option>2019</option>
+			<option>2018</option>
+			<option>2017</option>
+			<option>2016</option>
+			<option>2015</option>
+			<option>2014</option>
+			<option>2013</option>
+			<option>2012</option>
+			<option>2011</option>
+			<option>2010</option>
+			<option>2009</option>
+			<option>2008</option>
+			<option>2007</option>
+			<option>2006</option>
+			<option>2005</option>
+			<option>2004</option>
+			<option>2003</option>
+			<option>2002</option>
+			<option>2001</option>
+			<option>2000</option>
+			<option>1999</option>
+			<option>1998</option>
+			<option>1997</option>
+			<option>1996</option>
+			<option>1995</option>
+			<option>1994</option>
+			<option>1993</option>
+			<option>1992</option>
+			<option>1991</option>
+			<option>1990</option>
+			<option>1989</option>
+			<option>1988</option>
+			<option>1987</option>
+			<option>1986</option>
+			<option>1985</option>
+			<option>1984</option>
+			<option>1983</option>
+			<option>1982</option>
+			<option>1981</option>
+			<option>1980</option>
+			<option>1979</option>
+			<option>1978</option>
+			<option>1977</option>
+			<option>1976</option>
+			<option>1975</option>
+			<option>1974</option>
+			<option>1973</option>
+			<option>1972</option>
+			<option>1971</option>
+			<option>1970</option>
+			<option>1969</option>
+			<option>1968</option>
+			<option>1967</option>
+			<option>1966</option>
+			<option>1965</option>
+			<option>1964</option>
+			<option>1963</option>
+			<option>1962</option>
+			<option>1961</option>
+			<option>1960</option>
+			<option>1959</option>
+			<option>1958</option>
+			<option>1957</option>
+			<option>1956</option>
+			<option>1955</option>
+			<option>1954</option>
+			<option>1953</option>
+			<option>1952</option>
+			<option>1951</option>
+			<option>1950</option>
+			<option>1949</option>
+			<option>1948</option>
+			<option>1947</option>
+			<option>1946</option>
+			<option>1945</option>
+			<option>1944</option>
+			<option>1943</option>
+			<option>1942</option>
+			<option>1941</option>
+			<option>1940</option>
+			<option>1939</option>
+			<option>1938</option>
+			<option>1937</option>
+			<option>1936</option>
+			<option>1935</option>
+			<option>1934</option>
+			<option>1933</option>
+			<option>1932</option>
+			<option>1931</option>
+			<option>1930</option>
+			<option>1929</option>
+			<option>1928</option>
+			<option>1927</option>
+			<option>1926</option>
+			<option>1925</option>
+			<option>1924</option>
+			<option>1923</option>
+			<option>1922</option>
+			<option>1921</option>
+			<option>1920</option>
+			<option>1919</option>
+			<option>1918</option>
+			<option>1917</option>
+			<option>1916</option>
+			<option>1915</option>
+			<option>1914</option>
+			<option>1913</option>
+			<option>1912</option>
+			<option>1911</option>
+			<option>1910</option>
+			<option>1909</option>
+			<option>1908</option>
+			<option>1907</option>
+			<option>1906</option>
+			<option>1905</option>
+			<option>1904</option>
+			<option>1903</option>
+			<option>1902</option>
+			<option>1901</option>
+			<option>1900</option>
+			<option>1899</option>
+			<option>1898</option>
+			<option>1897</option>
+			<option>1896</option>
+			<option>1895</option>
+			<option>1894</option>
+			<option>1893</option>
+			<option>1892</option>
+			<option>1891</option>
+			<option>1890</option>
+			</select>
+			<br><br>
+
+			<input type='submit' name='submit' id='submit' value='CALCULATE'>
+		</form>
+	</body>
+</html>
+
+
+<?php
+if(isset($_POST['submit']))
+{
+	$b_day=$_POST['day'];
+	$b_month=$_POST['month'];
+	$b_year=$_POST['year'];
+	
+	
+	$c_year=date("Y");
+	
+	$c_m_alpha=date("M");
+	if($c_m_alpha=="Jan")
+	{
+		$c_month=1;
+		
+	}
+	elseif($c_m_alpha=="Feb")
+	{
+		$c_month=2;
+		
+	}
+	elseif($c_m_alpha=="Mar")
+	{
+		$c_month=3;
+		
+	}
+	elseif($c_m_alpha=="Apr")
+	{
+		$c_month=4;
+	
+	}
+	elseif($c_m_alpha=="May")
+	{
+		$c_month=5;
+	
+	}
+	elseif($c_m_alpha=="Jun")
+	{
+		$c_month=6;
+	
+	}
+	elseif($c_m_alpha=="Jul")
+	{
+		$c_month=7;
+		
+	}
+	elseif($c_m_alpha=="Aug")
+	{
+		$c_month=8;
+	
+	}
+	elseif($c_m_alpha=="Sep")
+	{
+		$c_month=9;
+		
+	}
+	elseif($c_m_alpha=="Oct")
+	{
+		$c_month=10;
+	
+	}
+	elseif($c_m_alpha=="Nov")
+	{
+		$c_month=11;
+	
+	}
+	elseif($c_m_alpha=="Dec")
+	{
+		$c_month=12;
+	
+	}
+	
+	$c_month_copy=$c_month;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	$date = new DateTime('now', new DateTimeZone('Asia/Kolkata'));
+ $c_day=$date->format('d');
+	
+	
+	if($b_day>$c_day && $b_month>$c_month && $b_year>$c_year)
+	{
+		echo "<span style='font-size:40px;color:coral;'>Please Select Valid Date</span>";
+	}
+	
+	else
+	{
+	
+	
+	if($b_month==2)
+	{
+		for($i=$b_year;$i<=$c_year;$i++)
+		{
+			if(($i/4)==0)
+			{
+				$s_b_days=29;
+			}
+			else
+			{
+				$s_b_days=28;
+			}
+		}
+	}
+	elseif($b_month==4 || $b_month==6 || $b_month==9 || $b_month==11)
+	{
+		$s_b_days=30;
+	}		
+	else
+	{
+		$s_b_days=31;
+	}
+	
+
+	
+	if($c_month_copy>$b_month)
+	{
+		$age_month=$c_month_copy-$b_month;
+	}
+	else
+	{
+		$age_month=$b_month-$c_month_copy;
+	}
+	
+	$count=0;
+	
+		$age_year=$c_year-$b_year;
+		
+	if($age_month<0)
+	{
+		$age_year=$age_year-1;
+		for($i=$b_month;$i<12;$i++)
+		{
+			$count=$count+1;
+		}
+		while($c_month_copy!=1)
+		{
+			$count=$count+1;
+			$c_month_copy=$c_month_copy-1;
+		}
+	}
+	
+	
+	elseif($age_month>0 && $b_month>$c_month_copy)
+	{
+		$age_year=$age_year-1;
+		for($i=$b_month;$i<12;$i++)
+		{
+			$count=$count+1;
+		}
+		while($c_month_copy!=0)
+		{
+			$count=$count+1;
+			$c_month_copy=$c_month_copy-1;
+		}
+
+	}
+
+	else
+	{
+		
+			
+		for($i=$b_month;$i<$c_month_copy;$i++)
+		{
+			$count=$count+1;
+		}
+		/**if($b_day>$c_day)
+		{
+			$count=$count-1;
+		}**/
+
+		
+	}	
+	
+	$age_day=$c_day-$b_day;	
+
+	echo "
+	
+	
+	<hmtl>
+	<br>
+	<div style='font-size:40px;'>
+	Your Date of Birth is : 
+	<span style='color:coral;'>".$b_day." / ".$b_month." / ".$b_year."</span>
+	<br>
+	Your Age is : 
+	<span style='color:coral;'><i>".$age_year."</i> years <i>" . $count."</i> months  ". $age_day ." days </span>
+	</div></center></html>";
+	
+	}
+
+	
+}
+
+
+
+
+?>
